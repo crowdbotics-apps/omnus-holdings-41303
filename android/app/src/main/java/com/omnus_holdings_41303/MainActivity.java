5c0bbda69bbb49d0db3b22342c1eaf41514e7461
Crowdbotics <team@crowdbotics.com>
2023-06-06 17:59:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/omnus_holdings_41303/MainActivity.java
@@ -0,0 +1,15 @@
+package com.omnus_holdings_41303;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "omnus_holdings_41303";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "omnus-holdings-41303",
+  url: "https://omnus-holdings-41303.botics.co",
+  api: "https://omnus-holdings-41303.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
